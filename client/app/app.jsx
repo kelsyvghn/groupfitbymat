68cb54b27aea2858d320c060ecbfb7c2c41d034c
Kelsy Vaughn <angelstate@gmail.com>
2020-07-07 10:53:27 -0700

    add resources and setup basic background and tabs

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import style from '../css/app.css';
 export default class App extends React.Component{
 
   render(){
     return (
-       <div>Hello App!</div>
+       <div className={style.home}>Hello App!</div>
     )
   }
 }
